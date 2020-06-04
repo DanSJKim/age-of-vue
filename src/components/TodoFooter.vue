c0cf52a127e440ee203ed3e4b0035658b9e1020d
Seungjune Kim <kimsj5@brandi.co.kr>
2020-06-04 20:50:45 +0900

    update vue intermediate codes

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <span>
+            <span v-on:click="clearTodo">Clear All</span>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            clearTodo: function() {
+                localStorage.clear();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<!--scoped는 해당 컴포넌트 안에서만 유효한 style속성으로 변환된다.-->
+<style scoped>
+    h1 {
+        color: #2f3b52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
